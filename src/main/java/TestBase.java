a6ad9c9570e1c4359b56ddc762ee06b31dcc9b55
Mateusz <mateusz@Witajs-MacBook-Pro.local>
2018-05-28 17:38:36 +0200

    add chromedriver, add setup driver method, adjust pom.xml

--- src/main/java/TestBase.java
+++ src/main/java/TestBase.java
@@ -1,11 +1,30 @@
-import java.sql.SQLOutput;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestBase {
 
-    // This class will be used to configure drivers initialization
-    // It also can have more generic methods ie. taking screenshots etc.
+    // Static declaration of WebDriver variable to be used by test classes
+    public static WebDriver driver;
 
+    public static WebDriver setupDriver() {
+
+        // TO-DO: add the method to choose the browser from initConfig.properties file
+        String browserType = "Chrome";
+
+        switch(browserType) {
+            case "Chrome":
+                System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver");
+                driver = new ChromeDriver();
+                break;
+            // TO-DO: add case for other drivers and default case.
+        }
+        // TO-DO: add implicit wait functionality
+        return driver;
+    }
+
+    // REMOVE this later - added only to test if driver works
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        setupDriver();
+        driver.navigate().to("https://www.google.com/");
     }
 }
