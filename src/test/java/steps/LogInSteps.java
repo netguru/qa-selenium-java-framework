ded999440eae75e17f1a1101603cffb3c57b15c4
Konrad Filipiak <konrad.filipiak@netguru.co>
2018-05-31 22:00:26 +0200

    finish login tests

--- src/test/java/steps/LogInSteps.java
+++ src/test/java/steps/LogInSteps.java
@@ -7,7 +7,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.testng.Assert;
-import pages.LogInPage;
+import pages.*;
 
 import java.io.IOException;
 
@@ -29,11 +29,10 @@ public class LogInSteps extends TestBase {
         tearDown();
     }
 
-    // Scenario Outline: Successful login as <user_type>
-    @When("^User provides correct email: \"([^\"]*)\" and password$")
-    public void user_provides_correct_email_and_password(String email) {
+    @When("^User provides email: \"([^\"]*)\" and password: \"([^\"]*)\"$")
+    public void user_provides_email_and_password(String email, String password) {
         logInPage.provideEmail(email);
-        logInPage.providePassword("password");
+        logInPage.providePassword(password);
     }
 
     @When("^User selects Log in button$")
@@ -41,17 +40,83 @@ public class LogInSteps extends TestBase {
         logInPage.clickLogInButton();
     }
 
-    @Then("^User is taken to the dashboard page$")
-    public void user_is_taken_to_the_dashboard_page() {
-        // TODO: create dasbhoard page and get the url from the object
-        Assert.assertTrue(driver.getCurrentUrl().equals("http://tutor24.ch.lemonfrog.staging.devguru.co/en/dashboard"));
+    @Then("^User is logged in and taken to the dashboard page$")
+    public void user_is_logged_in_and_taken_to_the_dashboard_page() throws IOException {
+        DashboardPage dashboardPage = new DashboardPage(driver);
+
+        Assert.assertTrue(dashboardPage.isInitialized());
     }
 
-//    // Scenario: Successful logout
-//    @Given("^User is logged in$")
-//    public void user_is_logged_in() {
-//        logInPage.logIn("consumer_unpaid@netguru.pl", "password", false);
-//    }
+    @Given("^User is logged in$")
+    public void user_is_logged_in() {
+        logInPage.logIn("consumer_unpaid@netguru.pl", "password", false);
+    }
 
+    @When("^User selects Logout button$")
+    public void user_selects_Logout_button() throws IOException {
+        DashboardPage dashboardPage = new DashboardPage(driver);
 
+        Assert.assertTrue(dashboardPage.isInitialized());
+
+        dashboardPage.getMainHeaderSection().logOut();
+    }
+
+    @Then("^User is redirected to home page$")
+    public void user_is_redirected_to_home_page() throws IOException {
+        HomePage homePage = new HomePage(driver);
+
+        Assert.assertTrue(homePage.isInitialized());
+    }
+
+    @Then("^User cannot access the dashboard$")
+    public void user_cannot_access_the_dashboard() throws IOException {
+        DashboardPage dashboardPage = new DashboardPage(driver);
+        dashboardPage.goTo();
+
+        Assert.assertTrue(logInPage.isInitialized());
+    }
+
+    @When("^User does not provide the credentials$")
+    public void user_does_not_provide_the_credentials() {
+        logInPage.clickLogInButton();
+    }
+
+    @Then("^User cannot login$")
+    public void user_cannot_login() {
+        Assert.assertTrue(logInPage.isInitialized());
+    }
+
+    @Then("^\"([^\"]*)\" alert shows up$")
+    public void alert_shows_up(String alert) {
+        Assert.assertTrue(alert.equals(logInPage.getAlertText()));
+    }
+
+    @Then("^\"([^\"]*)\" message shows up$")
+    public void message_shows_up(String alert) {
+        Assert.assertTrue(alert.equals(logInPage.getAlertText()));
+    }
+
+    @When("^User selects Forgot Password button$")
+    public void user_selects_Forgot_Password_button() {
+        logInPage.clickForgotPasswordButton();
+    }
+
+    @Then("^User is redirected to Forgot Password page$")
+    public void user_is_redirected_to_Forgot_Password_page() throws IOException {
+        ForgotPasswordPage forgotPasswordPage = new ForgotPasswordPage(driver);
+
+        Assert.assertTrue(forgotPasswordPage.isInitialized());
+    }
+
+    @When("^User selects Sign up now button$")
+    public void user_selects_Sign_up_now_button() {
+        logInPage.clickSignUpNowButton();
+    }
+
+    @Then("^User is redirected to Register page$")
+    public void user_is_redirected_to_Register_page() throws IOException {
+        SignUpPage signUpPage = new SignUpPage(driver);
+
+        Assert.assertTrue(signUpPage.isInitialized());
+    }
 }
--- src/test/resources/features/LogInFeature.feature
+++ src/test/resources/features/LogInFeature.feature
@@ -1,16 +1,13 @@
 Feature: Login
 
 Scenario Outline: Successful login as <user_type>
-    When User provides correct email: "<email>" and password
+    When User provides email: "<email>" and password: "<password>"
     And User selects Log in button
-    Then User is taken to the dashboard page
+    Then User is logged in and taken to the dashboard page
     Examples:
-        |   user_type                   |   email                         |
-        |   admin                       |   admin@netguru.pl              |
-        |   consumer with payment       |   consumer_unpaid@netguru.pl    |
-        |   consumer without payment    |   consumer_paid@netguru.pl      |
-        |   consumer with discounts     |   consumer_special@netguru.pl   |
-        |   provider                    |   provider@netguru.pl           |
+        |   user_type   |   email               | password    |
+        |   admin       |   admin@netguru.pl    | password    |
+        |   provider    |   provider@netguru.pl | password    |
 
 Scenario: Successful logout
     Given User is logged in
@@ -24,28 +21,21 @@ Scenario: Cannot login with empty credentials
     Then User cannot login
     And "Invalid login data." alert shows up
 
-Scenario: Cannot login with invalid email
-    When User provides invalid email and good password
+Scenario Outline: Cannot login with wrong credentials
+    When User provides email: "<email>" and password: "<password>"
     And User selects Log in button
     Then User cannot login
-    And "Invalid email or password." alert shows up
+    And "<alert>" message shows up
+    Examples:
+        |   email               |   password        |   alert                       |
+        |   invalid_email       |   password        |   Invalid email or password.  |
+        |   some@wrong.email    |   password        |   Invalid email or password.  |
+        |   provider@netguru.pl |   wrong_password  |   Invalid login data.         |
 
-Scenario: Cannot login with wrong email
-    When User provides wrong email and good password
-    And User selects Log in button
-    Then User cannot login
-    And "Invalid email or password." alert shows up
-
-Scenario: Cannot login with wrong password
-    When User provides good email and wrong password
-    And User selects Log in button
-    Then User cannot login
-    And "Invalid email or password." alert shows up
-
-Scenario: Selecting "Forgot Password?" button
-    When User selects "Forgot Password?" button
+Scenario: Selecting Forgot Password button
+    When User selects Forgot Password button
     Then User is redirected to Forgot Password page
 
-Scenario: Selecting "Sign up now" button
-    When User selects "Sign up now" button
+Scenario: Selecting Sign up now button
+    When User selects Sign up now button
     Then User is redirected to Register page
