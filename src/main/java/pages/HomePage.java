86a45c4207720c620355a6b8efd87454ec311af0
Konrad Filipiak <konrad.filipiak@netguru.co>
2018-06-07 16:50:38 +0200

    remove redundant import

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -4,7 +4,6 @@ import base.PageBase;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.How;
 
 import java.io.IOException;
 
