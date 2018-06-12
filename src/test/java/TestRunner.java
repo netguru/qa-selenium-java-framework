4125820c5854cc3afe07d1357434b4fd94dccd72
Mateusz <mateusz@Witajs-MacBook-Pro.local>
2018-06-12 18:15:04 +0200

    adjust testrunner with options, change test name, add verbose

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -1,13 +1,16 @@
 import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = {"src/test/resources/features"},
-    glue={"steps"},
-    tags = { "~@ignore" }
+        features = {"src/test/resources/features"}
+        , glue={"steps"}
+        , snippets = SnippetType.CAMELCASE
+        , monochrome = true
+        , tags = {}
 )
 
 public class TestRunner extends AbstractTestNGCucumberTests {
