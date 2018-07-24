c01afb74707cc86d632155a259dc97330dc68ee0
dorozynskim <31191662+dorozynskim@users.noreply.github.com>
2018-07-24 17:45:07 +0200

    properties loading changed

--- src/main/java/utilities/UtilitiesFunctions.java
+++ src/main/java/utilities/UtilitiesFunctions.java
@@ -18,6 +18,23 @@ public final class UtilitiesFunctions {
             log.warn("Failed to load " + fileName + " file.");
         }
 
+        for (String key : new String[]{
+                "BROWSER"
+                , "BASE_URL"
+                , "LANGUAGE"
+                , "COOKIE_NAME"
+                , "COOKIE_VALUE"
+                , "ADMIN_EMAIL"
+                , "PROVIDER_EMAIL"
+                , "PAID_CONSUMER_EMAIL"
+                , "UNPAID_CONSUMER_EMAIL"
+                , "SPECIAL_CONSUMER_EMAIL"
+                , "COMMON_PASSWORD"}) {
+            if (System.getProperty(key) != null) {
+                props.put(key, System.getProperty(key));
+            }
+        }
+
         return props;
     }
 
