f6ba0a0f15504ea186d63fc3a1f34003e03c8357
Wojciech Wieczorek <wojciech.wieczorek@netguru.co>
2018-09-28 13:49:05 +0200

    fix

--- src/test/java/steps/HomePageSteps.java
+++ src/test/java/steps/HomePageSteps.java
@@ -5,19 +5,21 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
+import pages.HomePage;
 
 public class HomePageSteps extends TestingBase {
+  private HomePage homePage;
   @Given("User opens browser")
-  public void userOpensBrowser (){
+  public void userOpensBrowser() {
   }
 
   @When("User navigates to homepage")
-  public void navigateToHomepageUrl () {
+  public void navigateToHomepageUrl() {
     pages.getHomePage().goTo();
   }
 
   @Then("User sees logo")
-  public void logoIsVisible () {
+  public void logoIsVisible() {
     Assert.assertTrue(pages.getHomePage().getLogo().isDisplayed());
   }
 }
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -1,7 +1,7 @@
 Feature: Login
   Scenario Outline: Successful login as <user>
     Given The user is on login page
-    When User logs in as <user>
+    When User logs in as "<user>"
     Then User is logged in
     Examples:
       |user|
