99bb5052746cf2c517029c44fb8e4e1a418f5aa5
Agnieszka Mamla <agnieszka.mamla@netguru.pl>
2019-06-11 08:53:28 +0200

    CR changes + some refactoring

--- src/main/java/managers/ZAPManager.java
+++ src/main/java/managers/ZAPManager.java
@@ -10,11 +10,12 @@ import utilities.PropertiesLoader;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class ZAPManager {
-    private static final Logger log = LogManager.getLogger(Logger.class.getName());
+    private static final Logger log = LogManager.getLogger(ZAPManager.class.getName());
     private PropertiesLoader propertiesLoader = new PropertiesLoader();
     private ClientApi clientApi;
 
@@ -36,19 +37,23 @@ public class ZAPManager {
 
     public void spiderScan(String targetUrl) {
         try {
-            log.info("Stared spider scan for " + targetUrl + " please wait");
+            //set it to prevent freezing scanner
+            clientApi.spider.setOptionMaxDepth(1);
+            log.info("Start scanning");
             ApiResponse resp = clientApi.spider.scan(targetUrl, null, null, null, null);
 
             // The scan now returns a scan id to support concurrent scanning
-            String scanid = ((ApiResponseElement) resp).getValue();
-            int progress;
+            String scanId = ((ApiResponseElement) resp).getValue();
+            int progress = 0;
+            int info = 0;
 
             // Poll the status until it completes
-            while (true) {
-                Thread.sleep(1000);
-                progress = Integer.parseInt(((ApiResponseElement) clientApi.spider.status(scanid)).getValue());
-                if (progress >= 100) {
-                    break;
+            while (progress < 100) {
+                Thread.sleep(5000);
+                progress = Integer.parseInt(((ApiResponseElement) clientApi.spider.status(scanId)).getValue());
+                if(progress % 10 == 0 && info != progress) {
+                    log.info("Current progress: " + progress);
+                    info = progress;
                 }
             }
 
@@ -56,36 +61,31 @@ public class ZAPManager {
             Thread.sleep(2000);
             log.info("Spider scan for " + targetUrl + " completed");
             saveHTMLReport(new String(clientApi.core.htmlreport(), StandardCharsets.UTF_8));
-        } catch (ClientApiException | InterruptedException e) {
+        } catch (Exception e) {
             log.error(e);
         }
     }
 
     private String getCurrentTime() {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH.mm.ss");
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH.mm.ss");
         LocalDateTime currentTime = LocalDateTime.now();
         return dateTimeFormatter.format(currentTime);
     }
 
     private String getReportDirectory() {
-        String os = System.getProperty("os.name").toLowerCase();
-        if (os.startsWith("Windows")) {
-            return "reports\\";
-        } else {
-            return "reports/";
-        }
+        String currentPath = System.getProperty("reportDirectory");
+        return Paths.get(currentPath, "reports").toString();
     }
 
     private void saveHTMLReport(String result) {
         String timeToPrint = getCurrentTime();
-        File target = new File(getReportDirectory() + "owasp_" + timeToPrint + ".html");
-        try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter(target));
+        File reportDirectory = new File(getReportDirectory());
+        reportDirectory.mkdir();
+        File target = new File(reportDirectory, String.format("owasp_%s.html", timeToPrint));
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
             writer.write(result);
-            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
